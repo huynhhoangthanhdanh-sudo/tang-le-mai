d0a80659a722975751810d199b58eb0184250563
Danh3426 <huynhhoangthanhdanh@gmail.com>
2026-01-27 16:28:11 +0700

    Add files via upload

--- Test/changeLink.js
+++ Test/changeLink.js
@@ -1,23 +1,60 @@
 function changeLink() {
   const hours = new Date().getHours();
+  const button = document.getElementById("click");
   const link = document.getElementById("link");
   const paragraph = document.getElementById("paragraph");
+  let Morning = [
+    "Good morning🌞",
+    "Have a beautiful day 🧡🧡",
+    "BELIEVE IN YOURSELF ❤️‍🔥❤️‍🔥❤️‍🔥",
+    "The present is today and today is a present🎁🎁🏆",
+  ];
+  let Noon = [
+    "50% of the day, 50% ahead 🕛🕛",
+    "Let's take a break 🎵🎵",
+    "Bon appétit 🍚🍚",
+    "Come back full of energy in the afternoon ⚽️⚽️⚽️",
+  ];
+  let Afternoon = [
+    "Good afternoon ✨",
+    "Time to go home 🚌🚌🚌",
+    "Enjoy the rest of your day 🌅🌅🌅",
+    "Don't forget to eat something 🍱🍱🍱",
+  ];
+  let BeforeNight = [
+    "Finally! After a long day 🌕🌕",
+    "How was your day? 🥰🥰",
+    "Today is almost over, just wrap up and prepare for another day",
+    "a few days left🚍🚍🚍",
+  ];
+  let Night = [
+    "Good night💤💤💤",
+    "Sleep well💤💤💤",
+    "Gud nite💤💤💤",
+    "99 - Nighty night",
+  ];
 
-  if (hours >= 5 && hours <= 10) {
+  if (hours >= 5 && hours < 10) {
     link.src = "https://www.youtube.com/embed/ofaAppWeHZA";
-    link.textContent = "Hello";
-    paragraph.textContent = "Good morning 🌞";
-  } else if (hours >= 10 && hours <= 15) {
+    button.textContent = "Hello";
+    paragraph.textContent = Morning[Math.floor(Math.random() * Morning.length)];
+  } else if (hours >= 10 && hours < 15) {
     link.src = "https://www.youtube.com/embed/ofaAppWeHZA";
-    link.textContent = "...";
-    paragraph.textContent = "Have a nice day!!!";
-  } else if (hours >= 15 && hours <= 19) {
+    button.textContent = "Play";
+    paragraph.textContent = Noon[Math.floor(Math.random() * Noon.length)];
+  } else if (hours >= 15 && hours < 20) {
     link.src = "https://www.youtube.com/embed/ofaAppWeHZA";
-    link.textContent = "Enjoy";
-    paragraph.textContent = "Good afternoon ✨";
+    button.textContent = "Enjoy";
+    paragraph.textContent =
+      Afternoon[Math.floor(Math.random() * Afternoon.length)];
+  } else if (hours >= 20 && hours < 22) {
+    link.src = "https://www.youtube.com/embed/ofaAppWeHZA";
+    button.textContent = "Relax";
+    paragraph.textContent =
+      BeforeNight[Math.floor(Math.random() * BeforeNight.length)];
   } else {
-    link.textContent = "Sweet dream";
-    paragraph.textContent = "Good night";
+    button.textContent = "😴😴😴😴";
+    paragraph.textContent = Night[Math.floor(Math.random() * Night.length)];
   }
 }
 changeLink();
