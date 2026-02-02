1a1a4d573977cea95deb201a6459c2d16710a7d7
Danh3426 <huynhhoangthanhdanh@gmail.com>
2026-02-02 20:49:29 +0700

    Add files via upload

--- Test/changeLink.js
+++ Test/changeLink.js
@@ -35,20 +35,20 @@ function changeLink() {
   ];
 
   if (hours >= 5 && hours < 10) {
-    link.src = "https://www.youtube.com/embed/aK745kFRUoA";
+    link.src = "https://www.youtube.com/embed/V_a1rIMWViY";
     button.textContent = "Hello";
     paragraph.textContent = Morning[Math.floor(Math.random() * Morning.length)];
   } else if (hours >= 10 && hours < 15) {
-    link.src = "https://www.youtube.com/embed/XVRK6mgm3yw";
+    link.src = "https://www.youtube.com/embed/iuw8fmMliSc";
     button.textContent = "Play";
     paragraph.textContent = Noon[Math.floor(Math.random() * Noon.length)];
   } else if (hours >= 15 && hours < 20) {
-    link.src = "https://www.youtube.com/embed/l0xXpCpbkuM";
+    link.src = "https://www.youtube.com/embed/tALf6Vl0QuI";
     button.textContent = "Enjoy";
     paragraph.textContent =
       Afternoon[Math.floor(Math.random() * Afternoon.length)];
   } else if (hours >= 20 && hours < 22) {
-    link.src = "https://www.youtube.com/embed/aGefBVEtYZ8";
+    link.src = "https://www.youtube.com/embed/U-FS27YI7w4";
     button.textContent = "Relax";
     paragraph.textContent =
       BeforeNight[Math.floor(Math.random() * BeforeNight.length)];
